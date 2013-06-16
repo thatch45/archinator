54e84d6d713c77bbabe7dea6392165b5990efbf7
Thomas S Hatch <thatch45@gmail.com>
2013-06-16 11:13:56 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='archinator',
+      version='0.5.0',
+      description='Virtual machine generator for ArchLinux',
+      author='Thomas S Hatch',
+      author_email='thatch45@gmail.com',
+      url='https://github.com/thatch45/archinator',
+      packages=['archinator'],
+      scripts=['scripts/archinator'],
+     )
+
